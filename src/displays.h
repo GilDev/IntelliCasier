a06b1b7ee1db87948d80e76da0ad5fe4820e615e
GilDev <gildev@gmail.com>
2016-02-09 19:27:26 +0100

    Documented important functions

--- src/displays.h
+++ src/displays.h
@@ -7,16 +7,60 @@
 extern LedControl matrix;
 extern LiquidCrystal_I2C lcd;
 
+/**
+ * LED matrix & LCD display initialization
+ */
 void displaysInit(void);
+
+/**
+ * Clear LED matrix & LCD display
+ */
 void clearDisplays(void);
 
+/**
+ * Scroll a text on the LED matrix
+ * Supported characters: [A-Za-z0-9]
+ * @param text Text to scroll
+ */
 void newMatrixScroll(const char *text);
+
+/**
+ * Stop scrolling text on the LED matrix
+ */
 void stopMatrixScroll(void);
+
+/**
+ * Print an image on the LED matrix
+ * @param img Array of chars representing an image to print
+ */
 void drawImage(byte *img);
 
+/**
+ * Print text on the LCD display
+ * @param x    Horizontal coordinate
+ * @param y    Vertical coordinate
+ * @param text Text to print
+ */
 #define printLcd(x, y, text) lcd.setCursor(x, y); lcd.print(text);
+
+ /**
+  * Clear text on an LCD display's line
+  * @param line Line to erase
+  */
 void clearLcdLine(byte line);
+
+/**
+ * Scroll a text on an LCD display's line
+ * @param text Text to scroll
+ * @param line Line to scroll the text on
+ * @param speed Time between individual scrolls in milliseconds
+ */
 void newLcdScroll(const char *text, byte line, unsigned short speed);
+
+/**
+ * Stop scrolling text on an LCD display's line
+ * @param line Line to stop scrolling on
+ */
 void stopLcdScroll(byte line);
 
 #endif
--- src/events.h
+++ src/events.h
@@ -11,16 +11,67 @@ typedef enum {
 
 typedef signed char TimerId;
 
+/**
+ * Initialize event handler
+ */
 void eventsInit(void);
+
+/**
+ * Check and trigger events
+ */
 void eventsUpdateLoop(void);
 
+/**
+ * Call a function when pressing a button
+ * @param button   Button to check
+ * @param callback Function to call when the event triggers
+ * @param data     Data to pass to the callback
+ */
 void setSingleClickHandler(ButtonId button, void (*callback)(byte data), byte data);
+
+/**
+ * Call a function after holding a button for a certain amount of time
+ * @param button   Button to check
+ * @param delay    Amount of time between pressing the button and trigerring the event in milliseconds
+ * @param callback Function to call when the event triggers
+ * @param data     Data to pass to the callback
+ */
 void setHoldClickHandler(ButtonId button, unsigned short delay, void (*callback)(byte data), byte data);
+
+/**
+ * Call a function every some amount of time while holding a button
+ * @param button   Button to check
+ * @param delay    Amount of time between event triggers in milliseconds
+ * @param callback Function to call when the event triggers
+ * @param data     Data to pass to the callback
+ */
 void setRepeatClickHandler(ButtonId button, unsigned short delay, void (*callback)(byte data), byte data);
 
+/**
+ * Call a function after a certain amount of time
+ * @param delay    Time to wait before trigerring the event in milliseconds
+ * @param callback Function to call when the event triggers
+ * @param repeat   True if the event never stops
+ * @param data     Data to pass to the callback
+ */
 TimerId registerTimerEvent(unsigned short delay, void (*callback)(byte data), bool repeat, byte data);
+
+/**
+ * Update the amount of time before trigerring a Timer event
+ * @param id Timer to update
+ * @param newDelay Time to wait before trigerring the event in milliseconds
+ */
 void rescheduleTimerEvent(TimerId id, unsigned short newDelay);
+
+/**
+ * Cancel a Timer event
+ * @param id Timer to cancel
+ */
 void cancelTimerEvent(TimerId id);
+
+/**
+ * Cancel all Timer events
+ */
 void cancelAllTimerEvents(void);
 
 #endif
