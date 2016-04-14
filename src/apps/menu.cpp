01bfeb70f92926c744252d3bf644b4a1606f3b34
GilDev <gildev@gmail.com>
2016-04-14 12:13:36 +0200

    Entirely redone menu

--- src/apps/menu.cpp
+++ src/apps/menu.cpp
@@ -12,9 +12,6 @@
 
 static void exitMenu(void);
 
-static byte menuSelection;
-static byte submenuSelection; // 0 if not in a submenu
-
 static byte returnIcon[8] = {
 	B00010000,
 	B00110000,
@@ -26,198 +23,200 @@ static byte returnIcon[8] = {
 	B00000000
 };
 
+struct selection {
+	byte text;
+	byte icon[8];
+	byte menuToOpen; // -1 = call callback() instead
+	void (*callback)(void);
+};
+
+byte currentMenu;
+byte currentSelection;
+
+static struct selection menuMain[] = {
+	{
+		.text = GAMES_1_PLAYER_LCD,
+		.icon = {
+			B00001000,
+			B00011000,
+			B00001000,
+			B00001000,
+			B00001000,
+			B00001000,
+			B00001000,
+			B00011100
+		},
+		.menuToOpen = 1
+	},
+	{
+		.text = GAMES_2_PLAYERS_LCD,
+		.icon = {
+			B00111100,
+			B01000010,
+			B00000010,
+			B00000100,
+			B00001000,
+			B00010000,
+			B00100000,
+			B01111110
+		},
+		.menuToOpen = 2
+	},
+	{
+		.text = ABOUT,
+		.icon = {
+			B00111100,
+			B01000010,
+			B00000010,
+			B00000100,
+			B00001000,
+			B00001000,
+			B00000000,
+			B00001000
+		},
+		.menuToOpen = -1,
+		.callback = showAbout
+	}
+};
+
+static struct selection menu1PlayerGames[] = {
+	{
+		.text = SNAKE,
+		.icon = {
+			B00111100,
+			B01000010,
+			B00000010,
+			B00000100,
+			B00001000,
+			B00001000,
+			B00000000,
+			B00001000
+		},
+		.menuToOpen = -1,
+		.callback = showSnake
+	},
+	{
+		.text = FLAPPYBIRD,
+		.icon = {
+			B00111100,
+			B01000010,
+			B00000010,
+			B00000100,
+			B00001000,
+			B00001000,
+			B00000000,
+			B00001000
+		},
+		.menuToOpen = -1,
+		.callback = showSnake
+	},
+	{
+		.text = BACK,
+		.icon = {
+			B00010000,
+			B00110000,
+			B01111100,
+			B00110010,
+			B00010010,
+			B01000010,
+			B00111100,
+			B00000000
+		},
+		.menuToOpen = 0,
+	}
+};
+static struct selection menu2PlayerGames[] = {
+	{
+		.text = PONG,
+		.icon = {
+			B00111100,
+			B01000010,
+			B00000010,
+			B00000100,
+			B00001000,
+			B00001000,
+			B00000000,
+			B00001000
+		},
+		.menuToOpen = -1,
+		.callback = showPong
+	},
+	{
+		.text = SNAKE,
+		.icon = {
+			B00111100,
+			B01000010,
+			B00000010,
+			B00000100,
+			B00001000,
+			B00001000,
+			B00000000,
+			B00001000
+		},
+		.menuToOpen = -1,
+		.callback = showSnake2
+	},
+	{
+		.text = BACK,
+		.icon = {
+			B00010000,
+			B00110000,
+			B01111100,
+			B00110010,
+			B00010010,
+			B01000010,
+			B00111100,
+			B00000000
+		},
+		.menuToOpen = 0,
+	}
+};
+
+static byte numberOfMenuItems[] = {
+	sizeof menuMain / sizeof *menuMain - 1,
+	sizeof menu1PlayerGames / sizeof *menu1PlayerGames - 1,
+	sizeof menu2PlayerGames / sizeof *menu2PlayerGames - 1
+};
+
+static struct selection *menu[] = {
+	menuMain,
+	menu1PlayerGames,
+	menu2PlayerGames
+};
+
 static void displayMenu(void)
 {
 	clearLcdLine(0);
 
-	if (submenuSelection == 0) {
-		switch (menuSelection) {
-			case 0:
-				newMatrixScroll(strings[GAMES_1_PLAYER_MATRIX]);
-				printLcd(8 - stringsSizes[GAMES_1_PLAYER_LCD] / 2, 0, strings[GAMES_1_PLAYER_LCD]);
-				printLcd(7 - stringsSizes[CONFIRM], 1, strings[CONFIRM]);
-				// Print arrow
-				lcd.setCursor(8, 1);
-				lcd.write(0);
-				break;
-			case 1:
-				newMatrixScroll(strings[GAMES_2_PLAYERS_MATRIX]);
-				printLcd(8 - stringsSizes[GAMES_2_PLAYERS_LCD] / 2, 0, strings[GAMES_2_PLAYERS_LCD]);
-				printLcd(7 - stringsSizes[CONFIRM], 1, strings[CONFIRM]);
-				// Print arrow
-				lcd.setCursor(8, 1);
-				lcd.write(0);
-				break;
-			case 2:
-				newMatrixScroll(strings[ABOUT]);
-				printLcd(8 - stringsSizes[ABOUT] / 2, 0, strings[ABOUT]);
-				printLcd(7 - stringsSizes[CONFIRM], 1, strings[CONFIRM]);
-				// Print arrow
-				lcd.setCursor(8, 1);
-				lcd.write(0);
-		}
-	} else {
-		switch (menuSelection) {
-			case 0:
-				switch (submenuSelection) {
-					case 1:
-						newMatrixScroll(strings[SNAKE]);
-						printLcd(8 - stringsSizes[SNAKE] / 2, 0, strings[SNAKE]);
-						printLcd(7 - stringsSizes[PLAY], 1, strings[PLAY]);
-						// Print arrow
-						lcd.setCursor(8, 1);
-						lcd.write(0);
-						break;
-					case 2:
-						newMatrixScroll(strings[FLAPPYBIRD]);
-						printLcd(8 - stringsSizes[FLAPPYBIRD] / 2, 0, strings[FLAPPYBIRD]);
-						printLcd(7 - stringsSizes[PLAY], 1, strings[PLAY]);
-						// Print arrow
-						lcd.setCursor(8, 1);
-						lcd.write(0);
-						break;
-					case 3:
-						newMatrixScroll(strings[RACE]);
-						printLcd(8 - stringsSizes[RACE] / 2, 0, strings[RACE]);
-						printLcd(7 - stringsSizes[PLAY], 1, strings[PLAY]);
-						// Print arrow
-						lcd.setCursor(8, 1);
-						lcd.write(0);
-						break;
-					case 4:
-						drawImage(returnIcon);
-						printLcd(8 - stringsSizes[BACK] / 2, 0, strings[BACK]);
-						printLcd(7 - stringsSizes[CONFIRM], 1, strings[CONFIRM]);
-						// Print arrow
-						lcd.setCursor(8, 1);
-						lcd.write(0);
-				}
-				break;
-			case 1:
-				switch(submenuSelection) {
-					case 1:
-						newMatrixScroll(strings[PONG]);
-						printLcd(8 - stringsSizes[PONG] / 2, 0, strings[PONG]);
-						printLcd(7 - stringsSizes[PLAY], 1, strings[PLAY]);
-						// Print arrow
-						lcd.setCursor(8, 1);
-						lcd.write(0);
-						break;
-					case 2:
-						newMatrixScroll(strings[SNAKE]);
-						printLcd(8 - stringsSizes[SNAKE] / 2, 0, strings[SNAKE]);
-						printLcd(7 - stringsSizes[PLAY], 1, strings[PLAY]);
-						// Print arrow
-						lcd.setCursor(8, 1);
-						lcd.write(0);
-						break;
-					case 3:
-						drawImage(returnIcon);
-						printLcd(8 - stringsSizes[BACK] / 2, 0, strings[BACK]);
-						printLcd(7 - stringsSizes[CONFIRM], 1, strings[CONFIRM]);
-						// Print arrow
-						lcd.setCursor(8, 1);
-						lcd.write(0);
-				}
-		}
-	}
+	//drawImage(menu[currentMenu][currentSelection].icon);
+	newMatrixScroll(strings[menu[currentMenu][currentSelection].text]);
+
+	printLcd(8 - stringsSizes[menu[currentMenu][currentSelection].text] / 2,
+		0,
+		strings[menu[currentMenu][currentSelection].text]);
 }
 
-static void left(byte data)
+static void changeSelection(byte right)
 {
-	if (submenuSelection == 0) {
-		if (menuSelection == 0)
-			menuSelection = 2;
-		else
-			menuSelection--;
+	if (right) {
+		if (currentSelection++ == numberOfMenuItems[currentMenu])
+			currentSelection = 0;
 	} else {
-		byte limit;
-		switch (menuSelection) {
-			case 0:
-				limit = 4;
-				break;
-
-			case 1:
-				limit = 3;
-		}
-		if (submenuSelection == 1)
-			submenuSelection = limit;
-		else
-			submenuSelection--;
+		if (currentSelection-- == 0)
+			currentSelection = numberOfMenuItems[currentMenu];
 	}
 
 	displayMenu();
 }
 
-static void right(byte data)
+static void enter(byte data)
 {
-	if (submenuSelection == 0) {
-		if (menuSelection == 2)
-			menuSelection = 0;
-		else
-			menuSelection++;
+	if (menu[currentMenu][currentSelection].menuToOpen == 0xFF) {
+		(*(menu[currentMenu][currentSelection].callback))();
 	} else {
-		byte limit;
-		switch (menuSelection) {
-			case 0:
-				limit = 4;
-				break;
-
-			case 1:
-				limit = 3;
-		}
-		if (submenuSelection == limit)
-			submenuSelection = 1;
-		else
-			submenuSelection++;
-	}
-
-	displayMenu();
-}
-
-static void menu(byte data)
-{
-	if (submenuSelection == 0) { // Entering submenu
-		if (menuSelection == 2) { // About screen
-			exitMenu();
-			showAbout();
-			return;
-		}
-
-		submenuSelection = 1;
-		printLcd(0, 1, F("        "));
+		currentMenu = menu[currentMenu][currentSelection].menuToOpen;
+		currentSelection = 0;
 		displayMenu();
-	} else {
-		// "Return" menu options
-		if (menuSelection == 0 && submenuSelection == 4 ||
-			menuSelection == 1 && submenuSelection == 3) {
-			submenuSelection = 0;
-			printLcd(0, 1, F("        "));
-			displayMenu();
-		} else { // App launch
-			exitMenu();
-			switch (menuSelection) {
-				case 0: // 1 player games
-					switch (submenuSelection) {
-						case 1:
-							showSnake();
-							break;
-
-						case 2:
-							showFlappyBird();
-					}
-					break;
-				case 1: // 2 players games
-					switch (submenuSelection) {
-						case 1:
-							showPong();
-							break;
-
-						case 2:
-							showSnake2();
-					}
-			}
-		}
 	}
 }
 
@@ -225,15 +224,19 @@ void showMenu(void)
 {
 	clearDisplays();
 
-	setSingleClickHandler(PLAYER1_LEFT, left, 0);
-	setSingleClickHandler(PLAYER2_LEFT, left, 0);
-	setSingleClickHandler(PLAYER1_RIGHT, right, 0);
-	setSingleClickHandler(PLAYER2_RIGHT, right, 0);
-	setSingleClickHandler(MENU, menu, 0);
+	printLcd(7 - stringsSizes[CONFIRM], 1, "Valider");
+	lcd.setCursor(8, 1);
+	lcd.write(0);
+
+	setSingleClickHandler(PLAYER1_LEFT, changeSelection, 0);
+	setSingleClickHandler(PLAYER2_LEFT, changeSelection, 0);
+	setSingleClickHandler(PLAYER1_RIGHT, changeSelection, 1);
+	setSingleClickHandler(PLAYER2_RIGHT, changeSelection, 1);
+	setSingleClickHandler(MENU, enter, 0);
 
 	screensaverDelay = DELAY_BEFORE_SCREENSAVER_IN_MENU * 1000;
-	menuSelection = 0;
-	submenuSelection = 0;
+
+	currentMenu = currentSelection = 0;
 
 	displayMenu();
 }
